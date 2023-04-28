dd0dc5598f9766dbc8d07c5da92b3be34e6ab071
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-28 15:37:44 -0700

    complete add and edit forms

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, redirect, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, db, Pet
+from forms import AddPetForm, EditPetForm
+
 
 app = Flask(__name__)
 
@@ -35,21 +37,47 @@ def show_homepage():
     return render_template('homepage.html', pets = pets)
 
 @app.route("/add", methods=["GET", "POST"])
-def add_snack():
-    """Snack add form; handle adding."""
+def show_add_pet():
+    """Pet add form; handle adding."""
 
     form = AddPetForm()
 
     if form.validate_on_submit():
         name = form.name.data
-        species = form.species.data
+        species = form.species.data.lower()
         photo_url = form.photo_url.data
         age = form.age.data
         notes = form.notes.data
 
-        flash(f"Added {name} at {price}")
+        pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f"Added {name}")
         return redirect("/add")
 
     else:
         return render_template(
-            "snack_add_form.html", form=form)
+            "pet-add-form.html", form=form)
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def show_edit_pet(pet_id):
+    """Pet edit form; handle editing."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+
+        db.session.commit()
+
+        flash(f"Edited {pet.name}")
+        return redirect(f"/{pet_id}")
+
+    else:
+        return render_template(
+            "pet-edit-form.html", form=form, pet=pet)
--- forms.py
+++ forms.py
@@ -3,12 +3,55 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, URLField,
                     BooleanField, SelectField, TextAreaField)
-
+from wtforms.validators import (InputRequired, URL,
+                    AnyOf, Optional)
 
 class AddPetForm(FlaskForm):
+    """Form for adding pets"""
 
-    name = StringField("Name")
-    species = StringField("Species")
-    photo_url = URLField("Profile Picture")
-    age = SelectField("Age Group")
-    notes = StringField("Notes")
+    name = StringField("Name",
+            validators=[InputRequired()])
+    species = StringField("Species",
+            validators=[
+                InputRequired(),
+                AnyOf(['dog','cat','porcupine']),
+            ])
+    photo_url = URLField("Profile Picture",
+            validators=[
+                Optional(),
+                URL(),
+            ])
+    age = SelectField("Age Group",
+            choices=[
+                ("baby"),
+                ("young"),
+                ("adult"),
+                ("senior")
+            ],
+            validators=[
+                InputRequired(),
+            ])
+
+    notes = StringField("Notes",
+            validators=[
+                Optional(),
+            ])
+
+class EditPetForm(FlaskForm):
+    """Form for editing pet information"""
+
+    photo_url = URLField("Profile Picture",
+            validators=[
+                Optional(),
+                URL(),
+            ])
+
+    notes = StringField("Notes",
+            validators=[
+                Optional(),
+            ])
+
+    available = BooleanField("Availability",
+            validators=[
+                Optional(),
+            ])
